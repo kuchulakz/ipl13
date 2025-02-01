753fe72d3bc43ad0234d57855c890f6ef56bdc84
username <wecp-dummy@gmail.com>
2025-02-01 11:58:00 +0000

    new

--- frontend/src/app/ipl/types/Cricketer.ts
+++ frontend/src/app/ipl/types/Cricketer.ts
@@ -1,10 +1,85 @@
-export class Cricketer {
+// export class Cricketer {
   
+//     cricketerId : number;
+//     teamId : number;
+//     cricketerName : string;
+//     age : number;
+//     nationality : string;
+//     experience : number;
+//     role : string;
+//     totalRuns : number;
+//     totalWickets : number;
 
+//     constructor(cricketerId:number , teamId:number , cricketerName:string , age:number ,nationality:string , experience:number , role:string , totalRuns:number , totalWickets:number){
+
+//         this.cricketerId = cricketerId;
+//         this.teamId = teamId;
+//         this.cricketerName = cricketerName;
+//         this.age = age;
+//         this.nationality = nationality;
+//         this.experience = experience;
+//         this.role = role;
+//         this.totalRuns = totalRuns;
+//         this.totalWickets = totalWickets;
+//     }
+
+//     displayInfo(){
+//         console.log(`Cricketer ID: ${this.cricketerId}`);
+//         console.log(`Team ID: ${this.teamId}`);
+//         console.log(`Cricketer Name: ${this.cricketerName}`);
+//         // console.log(`Age: ${this.age}`);
+//         // console.log(`Nationality: ${this.nationality}`);
+//         // console.log(`Experience: ${this.experience}`);
+//         // console.log(`Role: ${this.role}`);
+//         // console.log(`Total Runs: ${this.totalRuns}`);
+//         // console.log(`Total Wickets: ${this.totalWickets}`);
+//     }
+   
+// }
+
+
+
+export class Cricketer {
+ 
+    cricketerId:number;
+    teamId:number;
+    cricketerName:string;
+    age:number;
+    nationality:string;
+    experience:number;
+    role:string;
+    totalRuns:number;
+    totalWickets:number;
+ 
+ 
+    constructor(cricketerId:number,
+    teamId:number,
+    cricketerName:string,
+    age:number,
+    nationality:string,
+    experience:number,
+    role:string,
+    totalRuns:number,
+    totalWickets:number){
+        this.cricketerId=cricketerId;
+          this.teamId=teamId;
+          this.cricketerName=cricketerName;
+          this.age=age;
+          this.nationality=nationality;
+          this.experience=experience;
+          this.role=role;
+          this.totalRuns=totalRuns;
+          this.totalWickets=totalWickets;
+ 
+         
+      }
+ 
+      displayInfo(){
+          console.log(`Cricketer ID: ${this.cricketerId}`);
+          console.log(`Team ID: ${this.teamId}`);
+          console.log(`Cricketer Name: ${this.cricketerName}`);
+      }
    
 }
-
-
-
-
-
+ 
+ 
--- frontend/src/app/ipl/types/Team.ts
+++ frontend/src/app/ipl/types/Team.ts
@@ -1,4 +1,48 @@
+// export class Team {
+
+//     teamId : number;
+//     teamName : string;
+//     location : string;
+//     ownerName : string;
+//     establishmentYear : number;
+
+//     constructor(teamId:number , teamName:string , location:string , ownerName:string , establishmentYear:number){
+
+//         this.teamId = teamId;
+//         this.teamName = teamName;
+//         this.location = location;
+//         this.ownerName = ownerName;
+//         this.establishmentYear = establishmentYear;
+//     }
+
+//     displayInfo() : void {
+//         console.log(`Team ID: ${this.teamId}`);
+//         console.log(`Team Name: ${this.teamName}`);
+//         console.log(`Location: ${this.location}`);
+//         // console.log(`Owner Name: ${this.ownerName}`);
+//         // console.log(`Establishment Year: ${this.establishmentYear}`);
+//     }
+// }
+
 
 export class Team {
-  
-}
+    teamId:number;
+    teamName:string;
+    location:string;
+    ownerName:string;
+    establishmentYear:number
+   
+    constructor(teamId:number,teamName:string,location:string,ownerName:string,establishmentYear:number){
+          this.teamId=teamId;
+          this.teamName=teamName;
+          this.location=location;
+          this.ownerName=ownerName;
+          this.establishmentYear=establishmentYear;
+      }
+   
+      displayInfo(){
+          console.log(`Team ID: ${this.teamId}`);
+          console.log(`Team Name: ${this.teamName}`);
+          console.log(`Location: ${this.location}`);
+      }
+  }
